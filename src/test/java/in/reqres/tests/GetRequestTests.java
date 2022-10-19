23d6e418746100c3e4160d0b04f686c15d1820ef
Novikov Aleksei <3900676@gmail.com>
2022-10-19 22:42:41 +0300

    added rest api GET test

--- src/test/java/in/reqres/tests/GetRequestTests.java
+++ src/test/java/in/reqres/tests/GetRequestTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
@@ -14,6 +15,7 @@ public class GetRequestTests {
     @Test
     void listUsersTest() {
         List<UserDataPojoModel> users = given()
+                .log().uri()
                 .when()
                 .contentType(JSON)
                 .get("https://reqres.in/api/users?page=2")
@@ -21,8 +23,17 @@ public class GetRequestTests {
                 .log().status()
                 .log().body()
                 .extract().body().jsonPath().getList("data", UserDataPojoModel.class);
+        //users.forEach(x -> Assertions.assertTrue(x.getAvatar().contains(x.getId().toString())));
+        //users.forEach(x -> Assertions.assertTrue(x.getEmail().contains(x.getFirst_name().toLowerCase(Locale.ROOT))));
 
-        Assertions.assertTrue(users.stream().allMatch(n -> n.getAvatar().endsWith(".jpg")));
+        //Assertions.assertTrue(users.stream().allMatch(n -> n.getAvatar().endsWith(".jpg")));
+
+        List<String> avatars = users.stream().map(UserDataPojoModel::getAvatar).collect(Collectors.toList());
+        List<String> ids = users.stream().map(x -> x.getId().toString()).collect(Collectors.toList());
+        for (int i = 0; i < avatars.size(); i++) {
+            Assertions.assertTrue(avatars.get(i).contains(ids.get(i)));
+        }
+
 
     }
 }
